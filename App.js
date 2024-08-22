2b5e65f4114ca35d01cf413987ca53861a0f3cbd
Nirosha Avuthu <anirusha73@gmailcom>
2024-08-22 15:28:32 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ const LearningApp = () =>{
+    return (
+        <>
+        <div className="app">
+            Learning
+        </div>
+        </>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LearningApp />);
+    
